f8dd7f18bc993b62bf0612ff8c0eb0c64fb0376b
Rezacrown <aryhidayatfebriana@gmail.com>
2023-06-07 08:53:20 +0800

    final 006

--- app/db/index.js
+++ app/db/index.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 const { url_mongodb } = require("../config");
 
 // (3) connect ke MongoDB menggunakan konfigurasi yang telah kita import
-mongoose.connect(`${url_mongodb}`);
+mongoose.connect(
+  url_mongodb ||
+    "mongodb+srv://aryhidayatfebriana:LFQ68A6ZFuyenUsZ@semina2023.zrhzumh.mongodb.net/?retryWrites=true&w=majority"
+);
 
 // (4) simpan koneksi dalam constant db
 const db = mongoose.connection;
